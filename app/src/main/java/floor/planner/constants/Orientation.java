39bf7e919891f396c76257ea73690758b5eb1b51
jphillips03 <13894312+jphillips03@users.noreply.github.com>
2023-10-28 16:03:06 -0400

    chore: add more orientations
    We need to be able to distinguish direction i.e. east-west vs west east.

--- app/src/main/java/floor/planner/constants/Orientation.java
+++ app/src/main/java/floor/planner/constants/Orientation.java
@@ -2,6 +2,8 @@ package floor.planner.constants;
 
 public enum Orientation {
     EAST_WEST,
+    WEST_EAST,
     NORTH_SOUTH,
+    SOUTH_NORTH,
     COLUMN // not an orientation per se, but they are a special kind of wall
 }
