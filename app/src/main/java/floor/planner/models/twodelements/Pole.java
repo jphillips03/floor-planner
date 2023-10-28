3308b66e642c516f0847ee4d06e7a1149c672e50
jphillips03 <13894312+jphillips03@users.noreply.github.com>
2023-10-28 18:29:14 -0400

    chore: reduce size of pole

--- app/src/main/java/floor/planner/models/twodelements/Pole.java
+++ app/src/main/java/floor/planner/models/twodelements/Pole.java
@@ -19,7 +19,7 @@ public class Pole extends DrawableElement {
         gl.glColor3f(1f, 0f, 0f);
         gl.glBegin(GL2.GL_POLYGON);
 
-        double r = 0.25;
+        double r = 0.15;
         for(int i = 0; i < 360; i++) {
 			double radians = i*Math.PI/180;
 			gl.glVertex2d(Math.cos(radians)*r + this.point.getX() + 0.5, Math.sin(radians)*r + this.point.getY() + 0.5);
