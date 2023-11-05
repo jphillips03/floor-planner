7d1738e552322f6c7614caeaa34a5711be743c9a
jphillips03 <13894312+jphillips03@users.noreply.github.com>
2023-11-05 18:19:11 -0500

    chore: update aspect ratio
    Set aspect ratio to 16:9 (which is wasn't before), and use multiple of ratio (*2.5) to ensure there is enough padding around floor plan.

--- app/src/main/java/floor/planner/constants/AspectRatio.java
+++ app/src/main/java/floor/planner/constants/AspectRatio.java
@@ -2,16 +2,23 @@ package floor.planner.constants;
 
 /**
  * The AspectRatio enum defines the default aspect ratio to use when rendering
- * objects in OpenGL. Currently the aspect ratio is 16:9, but the ratio is
- * divided by 4 so there isn't too much extra padding added around objects.
+ * objects in OpenGL. Aspect ratio defined as 16:9 converted to 1.78:1 (since
+ * 16/9 = 1.777). Ratio values are multiplied by 2.5 to ensure there is padding
+ * around floor plan (*2 seems too small, but *2.5 seems ok).
  */
 public enum AspectRatio {
-    X(4f),
-    Y(2.25f);
+    X(1.78f),
+    Y(1f);
     
     public float value;
 
+    /**
+     * Sets the aspect ratio constant value with the given value. The value is
+     * multiplied by 2.5 to ensure padding around floor plan.
+     *
+     * @param value Value to set for constant.
+     */
     private AspectRatio(float value) {
-        this.value = value;
+        this.value = value * 2.5f;
     }
 }
