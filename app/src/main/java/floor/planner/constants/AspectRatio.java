d2cccd86be83a9dc5cdbcdd5430922c8888a5c8d
jphillips03 <13894312+jphillips03@users.noreply.github.com>
2023-11-06 10:16:04 -0500

    fix: padding around floor plan
    Increase padding around floor plan so there is still space around it on smaller screens (2.5 did not give any space around height of floor plan).

--- app/src/main/java/floor/planner/constants/AspectRatio.java
+++ app/src/main/java/floor/planner/constants/AspectRatio.java
@@ -3,8 +3,8 @@ package floor.planner.constants;
 /**
  * The AspectRatio enum defines the default aspect ratio to use when rendering
  * objects in OpenGL. Aspect ratio defined as 16:9 converted to 1.78:1 (since
- * 16/9 = 1.777). Ratio values are multiplied by 2.5 to ensure there is padding
- * around floor plan (*2 seems too small, but *2.5 seems ok).
+ * 16/9 = 1.777). Ratio values are multiplied by 2.75 to ensure there is padding
+ * around floor plan (*2 seems too small, but *2.75 seems ok).
  */
 public enum AspectRatio {
     X(1.78f),
@@ -14,11 +14,11 @@ public enum AspectRatio {
 
     /**
      * Sets the aspect ratio constant value with the given value. The value is
-     * multiplied by 2.5 to ensure padding around floor plan.
+     * multiplied by 2.75 to ensure padding around floor plan.
      *
      * @param value Value to set for constant.
      */
     private AspectRatio(float value) {
-        this.value = value * 2.5f;
+        this.value = value * 2.75f;
     }
 }
