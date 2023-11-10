384d6706e22861127dc5b8d4e40005699ddd66fe
jphillips03 <13894312+jphillips03@users.noreply.github.com>
2023-11-10 02:36:32 -0500

    refactor: remove unnecessary class name

--- app/src/main/java/floor/planner/util/jogl/objects/obj3d/Wall3D.java
+++ app/src/main/java/floor/planner/util/jogl/objects/obj3d/Wall3D.java
@@ -31,25 +31,22 @@ public class Wall3D extends Cube {
     private void scaleCube() {
         float[][] vertices = this.getVertices();
         if (this.orientation.equals(Orientation.EAST_WEST)) {
-            // shorten y plane and then move to center of row
-            vertices = Matrix.scaleY(vertices, 0.05f);
-            vertices = Matrix.translateY(vertices, this.row + 0.5f);
+            vertices = Matrix.translatePartialY(vertices, 0.45f, FRONT_FACE);
+            vertices = Matrix.translatePartialY(vertices, -0.45f, BACK_FACE);
         } else if (this.orientation.equals(Orientation.NORTH_SOUTH)) {
-            // shorten x plane and then move to center of row
-            vertices = Matrix.scaleX(vertices, 0.05f);
-            vertices = Matrix.translateX(vertices, this.col + 0.5f);
+            vertices = Matrix.translatePartialX(vertices, 0.45f, LEFT_FACE);
+            vertices = Matrix.translatePartialX(vertices, -0.45f, RIGHT_FACE);
         } else if (this.orientation.equals(Orientation.COLUMN)) {
-            vertices = Matrix.translatePartialY(vertices, 0.45f, Cube.FRONT_FACE);
-            vertices = Matrix.translatePartialY(vertices, -0.45f, Cube.BACK_FACE);
+            vertices = Matrix.translatePartialY(vertices, 0.45f, FRONT_FACE);
+            vertices = Matrix.translatePartialY(vertices, -0.45f, BACK_FACE);
 
-            vertices = Matrix.translatePartialX(vertices, 0.45f, Cube.LEFT_FACE);
-            vertices = Matrix.translatePartialX(vertices, -0.45f, Cube.RIGHT_FACE);
+            vertices = Matrix.translatePartialX(vertices, 0.45f, LEFT_FACE);
+            vertices = Matrix.translatePartialX(vertices, -0.45f, RIGHT_FACE);
         }
 
         // shorten z plane of wall and move it up above floor tile to prevent
         // overlap in rendering (which leads to grainy image)...
-        vertices = Matrix.scaleZ(vertices, 0.95f);
-        vertices = Matrix.translateZ(vertices, 0.05f);
+        vertices = Matrix.translatePartialZ(vertices, 0.05f, BOTTOM_FACE);
         this.setVertices(vertices);
     }
     
