7981fa98cf642afd79ea1164fbedd538364fdc12
jphillips03 <13894312+jphillips03@users.noreply.github.com>
2023-11-11 13:43:35 -0500

    chore: add normal
    Add normal so polygon should render properly with lighting.

--- app/src/main/java/floor/planner/util/jogl/objects/obj3d/DrawableElement3D.java
+++ app/src/main/java/floor/planner/util/jogl/objects/obj3d/DrawableElement3D.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import floor.planner.util.jogl.objects.Color;
 import floor.planner.util.jogl.objects.DrawableElement;
+import floor.planner.util.math.Vector;
 
 public abstract class DrawableElement3D extends DrawableElement {
     public Color color;
@@ -13,6 +14,13 @@ public abstract class DrawableElement3D extends DrawableElement {
     public void drawPolygon(GL2 gl, List<float[]> points) {
         gl.glColor3f(this.color.getRed(), this.color.getGreen(), this.color.getBlue());
         gl.glBegin(GL2.GL_POLYGON);
+        float[] normal = Vector.normal(
+            points.get(0),
+            points.get(1),
+            points.get(2),
+            points.get(3)
+        );
+        gl.glNormal3f(normal[0], normal[1], normal[2]);
         for (float[] point : points) {
             gl.glVertex3f(point[0], point[1], point[2]);
         }
