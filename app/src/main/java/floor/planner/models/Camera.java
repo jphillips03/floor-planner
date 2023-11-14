2570aa326eae9dc88648eae63fdc8cc2c9f657c0
jphillips03 <13894312+jphillips03@users.noreply.github.com>
2023-11-14 16:36:37 -0500

    chore: more camera updates
    Fix tilting camera up and down. While this mostly works you can only go up and down, not completely around like turning. Also update move to factor in tilt so we can move at an angle. There are still some issues with the camera jumping depending on current angle, likely because of how lookAt point is set after moving...

--- app/src/main/java/floor/planner/models/Camera.java
+++ app/src/main/java/floor/planner/models/Camera.java
@@ -18,39 +18,25 @@ public class Camera {
     private int height;
     private int numFloors;
 
-    // x and z position of the player, y is 0
     private float posX = 0;
     private float posY = 0;
     private float posZ = 0;
     private float lookAtX = 0;
     private float lookAtY = 0;
     private float lookAtZ = 0;
-    private float headingZ = 0; // heading of player, about y-axis
-    private float lookUpAngle = 0.0f;
     private float viewRadius;
-
     private float moveIncrement = 0.05f;
-    private float turnIncrement = 0.05f; // each turn in degree
-    private float lookUpIncrement = 1.0f;
-
     private int currentPoint = 0;
-    private int currentDegree = 90;
+    private int currentZPoint = 0;
     private float degreesBetweenPoints = 5f;
     private List<Point3D> pointsOnLookAtCircle;
+    private List<Point3D> pointsOnTiltCircle;
 
     public Camera(int width, int height, int numFloors) {
         this.width = width;
         this.height = height;
         this.numFloors = numFloors;
         this.init();
-    }
-
-    public float getLookUpAngle() {
-        return this.lookUpAngle;
-    }
-
-    public float getHeadingZ() {
-        return this.headingZ;
     }
 
     public float getPosX() {
@@ -90,18 +76,24 @@ public class Camera {
         this.initPointsOnCircle();
         // set look at point to face +y axis (1/4 of way around circle)
         this.currentPoint = this.pointsOnLookAtCircle.size() / 4;
-        this.currentDegree = 90;
+        this.currentZPoint = this.pointsOnTiltCircle.size() / 4;
         this.setLookAt(this.currentPoint);
     }
 
     private void initPointsOnCircle() {
         this.pointsOnLookAtCircle = new ArrayList<Point3D>();
-        // 0 is +x axis, 90 is +y axis, 180 is -x axis, and 270 is -y axis
+        this.pointsOnTiltCircle = new ArrayList<Point3D>();
         for (float d = 0; d < 360; d += this.degreesBetweenPoints) {
+            // 0 is +x axis, 90 is +y axis, 180 is -x axis, and 270 is -y axis
             float x = this.posX + (this.viewRadius * (float) Math.cos(Math.toRadians(d)));
             float y = this.posY + (this.viewRadius * (float) Math.sin(Math.toRadians(d)));
             Point3D p = new Point3D(x, y, this.posZ);
             this.pointsOnLookAtCircle.add(p);
+
+            // set up points around vertical circle for tilt
+            float z = this.posZ + (this.viewRadius * (float) Math.cos(Math.toRadians(d)));
+            p = new Point3D(this.posX, y, z);
+            this.pointsOnTiltCircle.add(p);
         }
     }
 
@@ -109,17 +101,19 @@ public class Camera {
         Point3D lookAtPoint = this.pointsOnLookAtCircle.get(i);
         this.lookAtX = lookAtPoint.getX();
         this.lookAtY = lookAtPoint.getY();
-        this.lookAtZ = lookAtPoint.getZ();
+        this.lookAtZ = this.pointsOnTiltCircle.get(this.currentZPoint).getZ();
     }
 
     public void lookUp() {
-        this.lookUpAngle -= this.lookUpIncrement;
-        this.lookAtZ++;
+        this.changeCurrentZPoint(-1);
+        this.lookAtX = this.pointsOnTiltCircle.get(this.currentZPoint).getX();
+        this.lookAtZ = this.pointsOnTiltCircle.get(this.currentZPoint).getZ();
     }
 
     public void lookDown() {
-        this.lookUpAngle += this.lookUpIncrement;
-        this.lookAtZ--;
+        this.changeCurrentZPoint(1);
+        this.lookAtX = this.pointsOnTiltCircle.get(this.currentZPoint).getX();
+        this.lookAtZ = this.pointsOnTiltCircle.get(this.currentZPoint).getZ();
     }
 
     public void moveIn() {
@@ -139,25 +133,82 @@ public class Camera {
      */
     private void move(int increment) {
         float t = moveIncrement / this.viewRadius;
-        
-        // determine point to move towards (either lookAt point or point on
-        // opposite end of circle depending on given increment)
-        float x2, y2;
-        if (increment > 0) {
-            // moving in so just use the lookAt point
-            x2 = this.lookAtX;
-            y2 = this.lookAtY;
+
+        // moving is determined based on tilt (up or down) and look angle
+        // (right or left); moving in XY plane or YZ plane is easy, moving at
+        // at tilt is most complicated (starting to see why most solutions
+        // just translate/rotate model instead of moving camera...)
+        if (
+            this.currentZPoint == this.pointsOnTiltCircle.size() / 4 ||
+            this.currentZPoint == this.pointsOnTiltCircle.size() * (3/4)
+        ) {
+            // we're moving flat, so just move in XY plane...
+            logger.info("Moving flat");
+            // determine point to move towards (either lookAt point or point on
+            // opposite end of circle depending on given increment)
+            float x2, y2;
+            if (increment > 0) {
+                // moving in so just use the lookAt point
+                x2 = this.lookAtX;
+                y2 = this.lookAtY;
+            } else {
+                // moving away so find the point opposite where we are looking and
+                // move toward that; see https://math.stackexchange.com/a/567081
+                x2 = 2 * this.posX - this.lookAtX;
+                y2 = 2 * this.posY - this.lookAtY;
+            }
+
+            // move along the line of sight (line between pos and lookAt) towards
+            // (x2, y2) as defined above
+            this.posX = (1 - t) * this.posX + t * x2;
+            this.posY = (1 - t) * this.posY + t * y2;
+        } else if (
+            this.currentPoint == 0 ||
+            this.currentPoint == this.pointsOnLookAtCircle.size() / 2
+        ) {
+            // we're moving up or down so just move in YZ plane
+            logger.info("Moving up/down");
+            // determine point to move towards (either lookAt point or point on
+            // opposite end of circle depending on given increment)
+            float z2, y2;
+            if (increment > 0) {
+                // moving in so just use the lookAt point
+                z2 = this.lookAtZ;
+                y2 = this.lookAtY;
+            } else {
+                // moving away so find the point opposite where we are looking and
+                // move toward that; see https://math.stackexchange.com/a/567081
+                z2 = 2 * this.posZ - this.lookAtZ;
+                y2 = 2 * this.posY - this.lookAtY;
+            }
+
+            // move along the line of sight (line between pos and lookAt) towards
+            // (z2, y2) as defined above
+            this.posZ = (1 - t) * this.posZ + t * z2;
+            this.posY = (1 - t) * this.posY + t * y2;
         } else {
-            // moving away so find the point opposite where we are looking and
-            // move toward that; see https://math.stackexchange.com/a/567081
-            x2 = 2 * this.posX - this.lookAtX;
-            y2 = 2 * this.posY - this.lookAtY;
+            // we're tilted...
+            float x2, y2, z2;
+            if (increment > 0) {
+                // moving in so just use the lookAt point
+                x2 = this.lookAtX;
+                y2 = this.lookAtY;
+                z2 = this.lookAtZ;
+            } else {
+                // moving away so find the point opposite where we are looking and
+                // move toward that; see https://math.stackexchange.com/a/567081
+                x2 = 2 * this.posX - this.lookAtX;
+                y2 = 2 * this.posY - this.lookAtY;
+                z2 = 2 * this.posZ - this.lookAtZ;
+            }
+
+            // move along the line of sight (line between pos and lookAt) towards
+            // (x2, y2) as defined above
+            this.posX = (1 - t) * this.posX + t * x2;
+            this.posY = (1 - t) * this.posY + t * y2;
+            this.posZ = (1 - t) * this.posZ + t * z2;
         }
 
-        // move along the line of sight (line between pos and lookAt) towards
-        // (x2, y2) as defined above
-        this.posX = (1 - t) * this.posX + t * x2;
-        this.posY = (1 - t) * this.posY + t * y2;
         this.initPointsOnCircle();
         this.setLookAt(this.currentPoint);
     }
@@ -176,21 +227,27 @@ public class Camera {
         if (delta < 0) {
             if (this.currentPoint == 0) {
                 this.currentPoint = this.pointsOnLookAtCircle.size() - 1;
-                this.currentDegree = 0;
             } else {
                 this.currentPoint--;
-                this.currentDegree--;
             }
         } else {
             if (this.currentPoint == this.pointsOnLookAtCircle.size() - 1) {
                 this.currentPoint = 0;
-                this.currentDegree = 0;
             } else {
                 this.currentPoint++;
-                this.currentDegree++;
             }
         }
+    }
 
-        logger.info("" + this.currentDegree);
+    private void changeCurrentZPoint(int delta) {
+        if (delta < 0) {
+            if (this.currentZPoint > 0) {
+                this.currentZPoint--;
+            }
+        } else {
+            if (this.currentZPoint < this.pointsOnTiltCircle.size() - 1) {
+                this.currentZPoint++;
+            }
+        }
     }
 }
