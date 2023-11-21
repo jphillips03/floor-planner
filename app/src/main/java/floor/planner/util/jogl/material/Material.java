1d216397b518f0b8d2f928a62618e54aec6821f7
jphillips03 <13894312+jphillips03@users.noreply.github.com>
2023-11-21 18:05:44 -0500

    chore: remove method
    Not all materials will have albedo (like glass...).

--- app/src/main/java/floor/planner/util/jogl/material/Material.java
+++ app/src/main/java/floor/planner/util/jogl/material/Material.java
@@ -1,10 +1,8 @@
 package floor.planner.util.jogl.material;
 
-import floor.planner.util.jogl.objects.Color;
 import floor.planner.util.jogl.raytracer.IntersectRecord;
 import floor.planner.util.math.Ray;
 
 public interface Material {
-    public Color getAlbedo();
     public ScatterAttenuation scatter(Ray rIn, IntersectRecord rec);
 }
