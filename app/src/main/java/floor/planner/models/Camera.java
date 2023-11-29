c1d5941d47bf21e5633b5392ce9e27e6240a6881
jphillips03 <13894312+jphillips03@users.noreply.github.com>
2023-11-29 17:38:06 -0500

    chore: update default setting
    Change the default setting based on RTIOW series.

--- app/src/main/java/floor/planner/models/Camera.java
+++ app/src/main/java/floor/planner/models/Camera.java
@@ -44,7 +44,7 @@ public class Camera {
     private double vFov = 50; // vertical view angle (field of view)
     private Vector u, v, w; // camera frame basis vectors
     private double defocusAngle = 0.6; // variation angle of rays through each pixel
-    private double focusDist = 2.5; // distance from camera lookFrom point to plane of perfect focus
+    private double focusDist = 10; // distance from camera lookFrom point to plane of perfect focus
     private Vector viewportU;
     private Vector viewportV;
     private Vector pixelDeltaU; // offset to pixel to right
