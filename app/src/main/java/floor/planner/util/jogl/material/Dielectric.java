ca0e8d3b2fd087e50c022bdff0193dd437e0c9a6
jphillips03 <13894312+jphillips03@users.noreply.github.com>
2023-12-19 19:24:49 -0500

    fix: schlick's approximation
    Replace * with + to match schlick's approximation. This fixes issue with reflecting rays from spheres.

--- app/src/main/java/floor/planner/util/jogl/material/Dielectric.java
+++ app/src/main/java/floor/planner/util/jogl/material/Dielectric.java
@@ -50,6 +50,6 @@ public class Dielectric extends Material {
     private static double reflectance(double cos, double refIdx) {
         double r0 = (1.0 - refIdx) / (1.0 + refIdx);
         r0 = r0 * r0;
-        return r0 * (1 - r0) * Math.pow((1 - cos), 5);
+        return r0 + (1 - r0) * Math.pow((1 - cos), 5);
     }
 }
